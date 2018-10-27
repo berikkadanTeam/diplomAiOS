d9accf0e3b1e73f9e27c62779f0bee15d3b27502
iamberikkadan <iamberikkadan@gmail.com>
2018-10-27 17:04:43 +0600

    Initial Commit

---
+++ diplomAiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  diplomAiOS
+//
+//  Created by Данияр on 10/27/18.
+//  Copyright © 2018 BookMe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
